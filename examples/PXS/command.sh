e131e2bcf9a086b342bde10f3fcf9da11969d485
Ricardo Amaya <53879471+RamayaACR@users.noreply.github.com>
2021-08-17 08:54:32 -0400

    Merge pull request #3 from ACRCode/feature/AILAB-1695
    Feature/AILAB-1695 - Interactive Confusion Matrix Support
    
    (cherry picked from commit ddd6df989c92bf87a997e0ee959d108376e0375f)

---
+++ examples/PXS/command.sh
@@ -0,0 +1,4 @@
+python src/main.py \
+    --dataset_file_path=".\examples\PXS\dataset.json" \
+    --output_file_path=".\examples\PXS\modelOutput.json" \
+    --evaluation_file_path=".\examples\PXS\evaluation.json"
