189c195d39bd24d602ee7d63a36c012887a341c9
Ricardo Amaya <ramaya@acr.org>
2023-08-18 09:21:21 -0400

    removed test code

--- hooks/hook-sklearn.__check_build._check_build.py
+++ hooks/hook-sklearn.__check_build._check_build.py
@@ -1,6 +1,5 @@
 from PyInstaller.utils.hooks import collect_submodules
 from PyInstaller.utils.hooks import collect_data_files
 
-print('hidden stuff wowowowowowowowowo')
 hiddenimports = collect_submodules('sklearn')
 datas = collect_data_files('sklearn')
